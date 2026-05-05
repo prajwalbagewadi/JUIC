700d3f36c5664d668968b98fd8a05ff433141461
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2026-05-05 10:41:12 +0530

    GWT imports succesful 05May2026

--- juic/JUIC/src/main/java/com/juic/Core/Juic.java
+++ juic/JUIC/src/main/java/com/juic/Core/Juic.java
@@ -1,6 +1,8 @@
 package com.juic.Core;
 
-import com.google.
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
 
 public class Juic {
 
